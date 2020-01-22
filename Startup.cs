26fd9f92513d915e009d5c357b58b719c4c5dfd3
Shows <cs330215@ohio.edu>
2020-01-22 11:16:09 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cs330215MIS4200_1045_sp20.Startup))]
+namespace cs330215MIS4200_1045_sp20
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
