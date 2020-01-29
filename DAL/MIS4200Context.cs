5a882b5a077682e0613bc7b82779a196d1b06587
Casey Shows <cs330215@ohio.edu>
2020-01-29 11:42:01 -0500

    Edits made in class (refer to SD2 doc)

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace cs330215MIS4200_1045_sp20.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base ("name=DefaultConnection")
+        {
+            // this method is a 'constructor' and is called when a new context is created
+        }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<customer> Customers { get; set; }
+    }
+}
