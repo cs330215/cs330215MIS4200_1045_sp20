8ec34483071f4d50c20718c51b7876ad7e48e8d4
Casey Shows <cs330215@ohio.edu>
2020-02-05 11:24:11 -0500

    AutomaticMigrationDataLossAllowed = true; (add this line to migration page when I create it)

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -11,12 +12,20 @@ namespace cs330215MIS4200_1045_sp20.DAL
         public MIS4200Context() : base ("name=DefaultConnection")
         {
             // this method is a 'constructor' and is called when a new context is created
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context,
+            cs330215MIS4200_1045_sp20.Migrations.MISContext.Configuration>("DefaultConnection"));
         }
         public DbSet<Orders> Orders { get; set; }
         public DbSet<customer> Customers { get; set; }
         public DbSet<Products> Products { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
 
+        // add this method - it will be used later
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
         // public System.Data.Entity.DbSet<cs330215MIS4200_1045_sp20.Models.Products> Products { get; set; }
     }
 }
